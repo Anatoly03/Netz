80cf3f6ca11afbcf07b02a35c8dca1af1f396780
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-09-18 19:53:37 +0200

    add read and write methods for bit7

--- network-library/src/bit7.rs
+++ network-library/src/bit7.rs
@@ -1,5 +1,3 @@
-
-
 //! Class to perform 7-bit integer decoding and encoding. Under '7-bit
 //! -encoding' is meant a representation of an integer as a pure byte
 //! buffer. The bytes are separated by their first bit, the 'flag' and
@@ -42,16 +40,68 @@ const HIGH8_MASK: u8 = 128;
 /// // Values above expected to be stored in five bytes
 /// assert_eq!(length_7bit(268435456), 5);
 /// ```
-pub fn length_7bit<T : std::cmp::PartialEq<i32> + std::ops::ShrAssign<i32>>(mut value: T) -> usize {
+pub fn length_7bit<T : std::cmp::PartialEq<usize> + std::ops::ShrAssign<usize>>(mut value: T) -> usize {
     let mut size = 0;
 
     loop {
-        value >>= 7;
+        value >>= 7usize;
         size += 1;
 
         if value == 0 {
             return size;
         }
     }
+}
+
+/// TODO This function is broken, the order of shifting seems incorrect, this function needs more edge case tests.
+/// 
+/// Reads in bytes of an integer in 7-bit encoding and decodes
+/// the number from the generating bytes. The callback function
+/// for providing the bytes will be called while the highest bit
+/// is set.
+///
+/// ```text
+/// 1111 0000 1010 1010 1000 0000 0000 0001 Reading In
+/// ^--- ---- ^--- ---- ^--- ---- ^--- ----
+///  111 0000  010 1010  000 0000  000 0001 Writing Out
+/// ```
+/// 
+/// ```
+/// use network_library::bit7::read_7bit;
+/// 
+/// // When the 7-bit follow flag is not set, returns the number.
+/// assert_eq!(read_7bit(&|| 0), 0);
+/// assert_eq!(read_7bit(&|| 1), 1);
+/// assert_eq!(read_7bit(&|| 127), 127);
+/// ```
+pub fn read_7bit(callback: &dyn Fn() -> u8) -> usize {
+    let mut value: usize = 0;
+    let mut shift: usize = 7;
+    const limit_bits: usize = std::mem::size_of::<usize>() * 7;
+
+    loop {
+        let byte = callback();
+        value |= (byte & LOWER7_MASK) as usize;
+        shift += 7;
+
+        if byte & HIGH8_MASK == 0 || shift > limit_bits {
+            return value;
+        }
+
+        value <<= 7;
+    }
+}
+
+/// TODO add tests
+/// 
+/// Yield bytes encoding an integer into a 7-bit integer elsewhere.
+/// This function will stream data to an function that accepts one
+/// byte (`u8`) as parameter.
+pub fn write_7bit(mut value: usize, callback: &dyn Fn(u8) -> ()) -> () {
+    while value as u8 >= HIGH8_MASK {
+        callback((((value as u8) & LOWER7_MASK) | HIGH8_MASK) as u8);
+        value >>= 7;
+    }
+    callback(value as u8);
 }
 
