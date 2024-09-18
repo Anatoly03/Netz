d643c70add618b0c01c77e4deb71cc4d1c37cc45
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-09-18 18:05:20 +0200

    add parse1 to expect input advancement

--- src/parser/interface.rs
+++ src/parser/interface.rs
@@ -1,4 +1,4 @@
-use nom::IResult;
+use nom::{error::{Error, ErrorKind}, Err, IResult};
 
 /// This trait describes a common trait that all network
 /// file parser elements have.
@@ -6,4 +6,16 @@ pub trait NetworkParser : Sized {
     /// Parse the given element using a `&str` input and
     /// `Self` output.
     fn parse(input: &str) -> IResult<&str, Self>;
+
+    /// Parse the given element using a `&str` input and
+    /// `Self` output, and expect to advance at least one
+    /// symbol.
+    fn parse1(input: &str) -> IResult<&str, Self> {
+        let (advanced_input, output) = Self::parse(input)?;
+        if input == advanced_input {
+            IResult::Err(Err::Error(Error::new(input, ErrorKind::Many1)))
+        } else {
+            IResult::Ok((advanced_input, output))
+        }
+    }
 }
