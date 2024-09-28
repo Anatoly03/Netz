ad3364dd1245c5a487dbeb3feb14865c2b1e3dc5
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-09-28 12:09:17 +0200

    add util-string tests

--- util-string/src/test/mod.rs
+++ util-string/src/test/mod.rs
@@ -1,7 +1,19 @@
 use super::*;
+use util_cases::CaseStyles;
 
 #[test]
 fn typescript_interface() {
     let mut builder = StringBuilder::from("");
-    builder += "export interface I";
+    let interface_name = "Example Interface";
+    let fields = vec![
+        ("fieldA", "FieldType"),
+        ("fieldB", "FieldType"),
+        ("fieldArray", "Fields[]"),
+    ];
+
+    builder += "export interface";
+    builder += format!("I{}", interface_name.to_pascal_case());
+    builder += "{";
+    
+    builder += "}";
 }
