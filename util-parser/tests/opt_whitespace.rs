1bb0d9a6eb03fd5b8a488056e817782bebfb4a85
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-10-01 22:11:56 +0200

    split tests

--- util-parser/tests/opt_whitespace.rs
+++ util-parser/tests/opt_whitespace.rs
@@ -1,16 +1,18 @@
-use util_parser::grammar;
+mod whitespace_opt {
+    use util_parser::grammar;
 
-#[grammar(~?)]
-pub struct OptionalWhitespace;
+    #[grammar(~?)]
+    pub struct OptionalWhitespace;
 
-#[test]
-fn whitespace() {
-    let (input, _) = OptionalWhitespace::parse("\t \r\n").unwrap();
-    assert_eq!(input, "");
-}
+    #[test]
+    fn whitespace() {
+        let (input, _) = OptionalWhitespace::parse("\t \r\n").unwrap();
+        assert_eq!(input, "");
+    }
 
-#[test]
-fn no_whitespace() {
-    let (input, _) = OptionalWhitespace::parse("").unwrap();
-    assert_eq!(input, "");
+    #[test]
+    fn no_whitespace() {
+        let (input, _) = OptionalWhitespace::parse("").unwrap();
+        assert_eq!(input, "");
+    }
 }
--- util-parser/tests/whitespace.rs
+++ util-parser/tests/whitespace.rs
@@ -1,15 +1,17 @@
-use util_parser::grammar;
+mod whitespace {
+    use util_parser::grammar;
 
-#[grammar(~)]
-pub struct Whitespace;
+    #[grammar(~)]
+    pub struct Whitespace;
 
-#[test]
-fn whitespace() {
-    let (input, _) = Whitespace::parse("\t \r\n").unwrap();
-    assert_eq!(input, "");
-}
+    #[test]
+    fn whitespace() {
+        let (input, _) = Whitespace::parse("\t \r\n").unwrap();
+        assert_eq!(input, "");
+    }
 
-#[test]
-fn no_whitespace() {
-    assert!(Whitespace::parse("").is_err());
+    #[test]
+    fn no_whitespace() {
+        assert!(Whitespace::parse("").is_err());
+    }
 }
