55ee713411ab7df8879696cf49281ec5b938b2c8
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-10-05 15:23:08 +0200

    refactor

--- util-nom-parser/src/attr/from.rs
+++ util-nom-parser/src/attr/from.rs
@@ -23,7 +23,7 @@ impl From<TokenStream> for Rule {
                     match proc_macro2::Literal::from_str(literal.to_string().as_ref()) {
                         Ok(lit) => match syn::Lit::new(lit) {
                             syn::Lit::Str(lit_str) => {
-                                vec.push(Rule::Keyword(lit_str.value()));
+                                vec.push(Self::Keyword(lit_str.value()));
                             }
                             // syn::Lit::ByteStr(lit_byte_str) => todo!(),
                             // syn::Lit::CStr(lit_cstr) => todo!(),
@@ -41,15 +41,17 @@ impl From<TokenStream> for Rule {
                     }
                 }
                 TokenTree::Punct(punct) => match punct.as_char() {
-                    '~' => vec.push(Rule::Whitespace),
+                    '~' => vec.push(Self::Whitespace),
                     '?' => match vec.pop() {
-                        Some(v) => vec.push(Rule::Option(Box::new(v))),
+                        Some(v) => vec.push(Self::Option(Box::new(v))),
                         None => {}, // TODO error: `?` was used at the beginning of a scope
                     }
                     '*' => match vec.pop() {
-                        Some(v) => vec.push(Rule::Repetition(Box::new(v))),
+                        Some(v) => vec.push(Self::Repetition(Box::new(v))),
                         None => {}, // TODO error: `*` was used at the beginning of a scope
                     }
+                    // TODO type cast: "a : B"
+                    // TODO range: "c - d"
                     _ => {}, // TODO error: expected one of `?` or `*`, got `{c}`
                 },
                 TokenTree::Ident(ident) => {
@@ -59,13 +61,13 @@ impl From<TokenStream> for Rule {
                     let rl = loop {
                         break match iter.next() {
                             // Identifiers that start with an uppercase letter are type references.
-                            Some(c) if c.is_ascii_uppercase() => Rule::TypeReference(st),
+                            Some(c) if c.is_ascii_uppercase() => Self::TypeReference(st),
                             // Identifiers that start with a lowercase letter are field identifiers.
-                            Some(c) if c.is_ascii_lowercase() => Rule::Identifier(st),
+                            Some(c) if c.is_ascii_lowercase() => Self::Identifier(st),
                             // Identifiers that start with `_` inherit the kind from the first letter.
                             Some(_) => continue,
                             // TODO If we reach the end, we should decide if it is a variable or panic.
-                            None => Rule::Identifier(st),
+                            None => Self::Identifier(st),
                         };
                     };
 
@@ -77,7 +79,9 @@ impl From<TokenStream> for Rule {
                             let subcontext = Self::from(group.stream());
                             vec.push(subcontext);
                         }
-                        del => {} //TODO panic!("expected parenthesis, got {del:?}"),
+                        Delimiter::Brace => todo!("expected paranthesis, got (...)"), // TODO proper error message
+                        Delimiter::Bracket => todo!("expected paranthesis, got [...]"), // TODO
+                        Delimiter::None => todo!("expected paranthesis, got delimiter from macro variable"), // TODO
                     }
                 }
             }
@@ -86,7 +90,7 @@ impl From<TokenStream> for Rule {
         // println!("attr: \"{attrs:?}\"");
 
         // todo!()
-        Rule::Scope(vec)
+        Self::Scope(vec)
 
         // compile_error!("Not implemented")
     }
